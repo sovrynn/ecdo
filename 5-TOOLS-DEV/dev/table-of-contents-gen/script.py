c5fca4431f0c19e05206f48a133d7a952293ec69
sovrynn <sovrynn>
2024-12-01 23:18:30 +0200

    fix corliss toc links

--- 5-TOOLS-DEV/dev/table-of-contents-gen/script.py
+++ 5-TOOLS-DEV/dev/table-of-contents-gen/script.py
@@ -40,7 +40,7 @@ def main():
     markdown_links = []
     for header_text in headers:
         # Remove all non-textual characters (except dashes and numbers), lowercase, and replace spaces with dashes
-        header_text_processed = re.sub(r'[^A-Za-z0-9\s-]', '', header_text)
+        header_text_processed = re.sub(r'[^A-Za-z0-9\s-]|(?:\s*\[.*?\]\(.*?\)\s*$)', '', header_text)
         header_text_processed = header_text_processed.lower()
         Y = header_text_processed.replace(' ', '-')
         link_url = f"https://github.com/sovrynn/ecdo/blob/master/{X_relative_to_ecdo}#{Y}-top"
